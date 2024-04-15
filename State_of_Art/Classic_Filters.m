a4ca498772d1e8c9c9484158e017252630368d71
SzymonMs <84814415+SzymonMs@users.noreply.github.com>
2024-04-15 21:56:21 +0200

    Wavelt transform

--- State_of_Art/Classic_Filters.m
+++ State_of_Art/Classic_Filters.m
@@ -3,11 +3,10 @@ clear;
 myDir = 'data';
 resultDirAverage = 'Average_Filter';
 resultDirMedian = 'Median_Filter';
-% resultDirWavelt = 'Wavelt_Filter';
 filePattern = fullfile(myDir);
 myFiles = dir(filePattern);
-n = 3;
-m = 3;
+n = 9;
+m = 9;
 for k = 1:length(myFiles)
       baseFileName = myFiles(k).name;
       fullFileName = fullfile(myDir, baseFileName);
@@ -19,14 +18,10 @@ for k = 1:length(myFiles)
         C_avg = fspecial('average',[n m]);
         denoisedI_avg = imfilter(I,C_avg);
         denoisedI_med = medfilt2(I,[n m]);
-        % denoisedI_wavelt=wdenoise2(im2uint8(I),3,'Wavelet','coif5','NoiseEstimate','LevelDependent','DenoisingMethod','Bayes','ThresholdRule','Soft','NoiseDirection',["h","v","d"]);
-        % denoisedI_wavelt = wdenoise2(I,2,'wavelet',"haar");
         fprintf('%d. %s\n',k-2, baseFileName);
         denoisedName_avg = strcat(resultDirAverage,"\Avg_denoised_",baseFileName);
         denoisedName_med = strcat(resultDirMedian,"\Med_denoised_",baseFileName);
-        % denoisedName_wavelt = strcat(resultDirWavelt,"\Wavelt_denoised_",baseFileName);
         imwrite(denoisedI_avg,denoisedName_avg);
         imwrite(denoisedI_med,denoisedName_med);
-        % imwrite(denoisedI_wavelt,denoisedName_wavelt);
       end
 end
---
+++ State_of_Art/Wavelt_Filter.m
@@ -0,0 +1,36 @@
+clc;
+clear;
+myDir = 'data';
+resultDirWavelt = 'Wavelt_Filter';
+filePattern = fullfile(myDir);
+myFiles = dir(filePattern);
+for k = 1:length(myFiles)
+      baseFileName = myFiles(k).name;
+      fullFileName = fullfile(myDir, baseFileName);
+      if length(baseFileName) > 3
+        I = imread(fullFileName) ;
+        if size(I, 3) == 3
+            I = rgb2gray(I);
+        end
+        I = double(I);
+        for x = 1:length(I(:,1))
+            for y = 1:length(I(1,:))
+                I(x,y) = log(I(x,y));
+                if abs(I(x,y)) > 5
+                    I(x,y) = 5;
+                end
+            end
+        end
+        dwtmode('per');
+        [thr,sorh,keepapp] = ddencmp('den','wv',I);
+        denoisedI_wavelt = wdencmp('gbl',double(I),'sym4',15,thr,sorh,keepapp);
+        fprintf('%d. %s\n',k-2, baseFileName);
+        denoisedName_wavelt = strcat(resultDirWavelt,"\Wavelt_denoised_",baseFileName);
+        for x = 1:length(denoisedI_wavelt(:,1))
+            for y = 1:length(denoisedI_wavelt(1,:))
+                denoisedI_wavelt(x,y) = exp(denoisedI_wavelt(x,y));
+            end
+        end
+        imwrite(uint8(denoisedI_wavelt),denoisedName_wavelt);
+      end
+end
